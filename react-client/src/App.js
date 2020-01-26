0e960fefe7add3facd5ec58458c3bb10f1ce045a
martinbozinovski <bozinovskimartin@gmail.com>
2020-01-26 20:55:06 +0100

    set up redux

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -8,8 +8,10 @@ import AddProject from './components/Project/AddProject';
 import UpdateProject from './components/Project/UpdateProject';
 import ProjectBoard from './components/ProjectBoard/ProjectBoard';
 import AddProjectTask from './components/ProjectBoard/ProjectTasks/AddProjectTask';
+import UpdateProjectTask from './components/ProjectBoard/ProjectTasks/UpdateProjectTask';
 import { Provider } from 'react-redux';
 import store from './store';
+
 
 
 
@@ -24,6 +26,7 @@ function App() {
           <Route exact path="/updateProject/:id" component={UpdateProject} /> 
           <Route exact path="/projectBoard/:id" component={ProjectBoard} />
           <Route exact path="/addProjectTask/:id" component={AddProjectTask} />
+          <Route exact path="/updateProjectTask/:backlog_id/:pt_id" component={UpdateProjectTask} />
         </div>
       </BrowserRouter>
     </Provider>
